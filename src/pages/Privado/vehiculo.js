c9aa94b1a271a4ce5028ff14e6ee6cad9ab1ea69
DanielC001 <danielcaro1000@gmail.com>
2022-01-10 17:16:37 -0500

    cur

--- src/pages/Privado/vehiculo.js
+++ src/pages/Privado/vehiculo.js
@@ -16,7 +16,7 @@ const Vehiculos = () => {
 
     useEffect(() => {
         const obtenerVeh = async () => {
-            const options = { method: 'GET', url: 'http://localhost:5000/vehiculos/' };
+            const options = { method: 'GET', url: 'https://lit-caverns-69036.herokuapp.com//vehiculos/' };
             await axios
                 .request(options)
                 .then(function (response) {
@@ -102,7 +102,7 @@ const FormularioCreacionVehiculos = ({ setMostrarTabla, listaVehiculos, setVehic
         });
         const options = {
             method: 'POST',
-            url: 'http://localhost:5000/vehiculos/nuevo/',
+            url: 'https://lit-caverns-69036.herokuapp.com//vehiculos/nuevo/',
             headers: { 'content-type': 'application/json' },
             data: { name: nuevoVehiculo.name, marca: nuevoVehiculo.marca },
         };
